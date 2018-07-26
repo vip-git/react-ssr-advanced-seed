7ef66c6bcda2ac8eef286e899d6705bcf6da5f48
Vipin Tanna <6302771+vip-git@users.noreply.github.com>
2018-07-26 15:17:45 +0200

    fix: docker build configured

---
+++ scripts/docker-build.js
@@ -0,0 +1,14 @@
+const shell = require('shelljs');
+
+shell.cd('../docker/prod');
+
+if (process.argv.slice(2).length && process.argv.slice(2)[0] === '--first') 
+{
+    shell.exec('docker build -t vipgit/react-ssr-nginx:'+ process.env.npm_package_version +' . && docker run -d -p 8080:80 --name=react-ssr-docker vipgit/react-ssr-nginx:'+ process.env.npm_package_version, function(code, stdout, stderr) {
+        console.log('Docker Build Successful - version deployed : vipgit/react-ssr-nginx:'+ process.env.npm_package_version);
+    });
+} else {
+    shell.exec('docker build -t vipgit/react-ssr-nginx:'+ process.env.npm_package_version +' . && docker rm react-ssr-docker --force && docker run -d -p 8080:80 --name=react-ssr-docker vipgit/react-ssr-nginx:'+ process.env.npm_package_version, function(code, stdout, stderr) {
+        console.log('Docker Build Successful - version deployed : vipgit/react-ssr-nginx:'+ process.env.npm_package_version);
+    });
+}
