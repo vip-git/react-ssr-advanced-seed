7af067bffa353cefef322f07aed5cce42831c0bc
vipin <6302771+vip-git@users.noreply.github.com>
2018-10-22 01:10:19 +0200

    fix: wallaby frontend unit test integration

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,18 +1,22 @@
-module.exports = function () {
+module.exports = function (wallaby) {
     return {
         files: [
             'tsconfig.json',
             'package.json',
             { pattern: 'src/server/**/*.ts', load: false },
-            { pattern: 'src/server/**/*.spec.ts', ignore: true }
+            { pattern: 'src/server/**/*.spec.ts', ignore: true },
+            { pattern: 'src/client/**/*.js', load: false },
+            { pattern: 'src/client/**/*.test.js', ignore: true }
         ],
         tests: [
             { pattern: 'src/server/**/*.spec.ts' },
+            { pattern: 'src/client/**/*.test.js' },
         ],
         env: {
             type: 'node',
             runner: 'node'
         },
+        compilers: {'src/client/**/*.js': wallaby.compilers.babel()},
         testFramework: 'jest',
         debug: true
     };
