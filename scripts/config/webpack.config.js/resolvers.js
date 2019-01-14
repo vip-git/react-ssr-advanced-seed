efd6b68f52391734dda4f8d6fc75cb4769e4bd40
Vipin Tanna <6302771+vip-git@users.noreply.github.com>
2019-01-14 16:39:22 +0100

    feat: absolute import resolvers

--- scripts/config/webpack.config.js/resolvers.js
+++ scripts/config/webpack.config.js/resolvers.js
@@ -1,6 +1,15 @@
 const paths = require('../paths');
+const path = require('path');
 
 module.exports = {
   extensions: ['.js', '.mjs', '.json', '.jsx', '.css'],
   modules: paths.resolveModules,
+  alias: {
+      "@services": path.resolve(__dirname, '../../../src/client/app/common/services'),
+      "@components": path.resolve(__dirname, '../../../src/client/app/common/components'),
+      "@config": path.resolve(__dirname, '../../../src/client/app/common/config'),
+      "@utils": path.resolve(__dirname, '../../../src/client/app/common/utils'),
+      "@mocks": path.resolve(__dirname, '../../../src/client/app/common/mocks'),
+      "@styles": path.resolve(__dirname, '../../../src/client/app/common/styles'),
+  },
 };
