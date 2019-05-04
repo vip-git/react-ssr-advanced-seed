2a7c6bfc373fd208d79c5368d662b8f24e4d4262
Vipin <6302771+vip-git@users.noreply.github.com>
2019-05-05 01:13:36 +0200

    fix: issues resolved with vulneribilities

--- scripts/config/webpack.config.js/loaders.js
+++ scripts/config/webpack.config.js/loaders.js
@@ -5,6 +5,40 @@ const babelLoader = {
   test: /\.(ts|js|jsx|tsx)$/,
   exclude: /node_modules/,
   loader: 'babel-loader',
+  options: {
+    "compact": true,
+    "presets": [
+        [
+            "@babel/preset-env",
+            {
+                "modules": false,
+                "targets": {
+                    "browsers": ["last 2 versions", "ie >= 9"]
+                }
+            }
+        ],
+        "@babel/typescript",
+        "@babel/preset-react",
+        "@babel/preset-flow",
+    ],
+    "plugins": [
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-syntax-dynamic-import"
+    ],
+    "env": {
+        "test": {
+            "plugins": [
+              "@babel/plugin-transform-modules-commonjs",
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-optional-chaining",
+              "@babel/plugin-syntax-dynamic-import"
+            ]
+        }
+    }
+  }
 };
 
 const cssLoaderClient = {
