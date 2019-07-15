ceaa2cf56fddd840840f92dd715b2abeaf98d147
Vipin <6302771+vip-git@users.noreply.github.com>
2019-07-15 02:26:41 +0200

    fix: clean up :white_check_mark:

--- src/client/shared/state/containers/app/__tests__/reducer.test.ts
+++ src/client/shared/state/containers/app/__tests__/reducer.test.ts
@@ -2,9 +2,18 @@ import reducer, { initialState } from '../reducer';
 import { ActionTypes } from '../actions';
 
 describe('App Reducer', () => {
-  it('sets the locale', () => {
-    expect(reducer(initialState, { type: ActionTypes.SETLOCALE, payload: 'de-DE' })).toEqual({
-      locale: 'de-DE',
-    });
-  });
+	it('can get Initial State', () => {
+		const initialState = {
+			locale: 'en-US'
+		};
+		const state = reducer(undefined, {});
+		expect(state).toStrictEqual(initialState);
+	});
+	it('sets the locale', () => {
+		expect(
+			reducer(initialState, { type: ActionTypes.SETLOCALE, payload: 'de-DE' })
+		).toEqual({
+			locale: 'de-DE'
+		});
+	});
 });
--- src/client/shared/utils/rules.engine.ts
+++ src/client/shared/utils/rules.engine.ts
@@ -6,19 +6,19 @@ import { catchError, filter } from 'rxjs/operators';
 
 // This should be a library eventually
 export class RulesEngine {
-  /* ignore coverage */
-  static applyRule = (action$, actionType, rules, successCallback) => 
-    action$.pipe(
-        ofType(actionType),
-        filter((action) => {
-          const processRules = rules(action);
-          const processedRuleErrors = _.filter(processRules, { error: true });
-          if (process.env.NODE_ENV === 'development' && processRules && processedRuleErrors.length) {
-            processedRuleErrors.map(err => console.warn('Some Rules are failing - ', err.message));
-          }
-          return processRules && processedRuleErrors.length === 0;
-        }),
-        ...successCallback(),
-        catchError(err => throwError(`Some Rules are failing - ${err.stack}`)),
-    );
+	/* ignore coverage */
+	static applyRule = (action$, actionType, rules, successCallback) =>
+		action$.pipe(
+			ofType(actionType),
+			filter(action => {
+				const processRules = rules(action);
+				const processedRuleErrors = _.filter(processRules, { error: true });
+				// if (process.env.NODE_ENV === 'development' && processRules && processedRuleErrors.length) {
+				//   processedRuleErrors.map(err => console.warn('Some Rules are failing - ', err.message));
+				// }
+				return processRules && processedRuleErrors.length === 0;
+			}),
+			...successCallback(),
+			catchError(err => throwError(`Some Rules are failing - ${err.stack}`))
+		)
 }
