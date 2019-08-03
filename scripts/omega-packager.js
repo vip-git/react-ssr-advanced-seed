9dcc560e472156e1d88f050052dca79e363da64e
Vipin <6302771+vip-git@users.noreply.github.com>
2019-08-03 10:33:11 +0200

    fix: packager information added

--- scripts/omega-packager.js
+++ scripts/omega-packager.js
@@ -7,10 +7,39 @@
  *  - omega package webChatContainer --container
  *  - omega package webChatComponent --component
  * - Give the possiblity to unpackage a container based on given json file.
- * 
+ *
  * Tasks:
  * - Read web container json and scafold a container
  * - Read web component json and scafold a component
  * - Read libraries from each json and do npm install with their specified version
  * - Symlink internal components so they can be imported and packaged externally.
  */
+
+function publishLernaRepos() {
+	/***
+	 * Lerna repos
+	 * - @omega-web-components
+	 *    - @omega-web-components/chat-box
+	 *    - @omega-web-components/doc-gen
+	 *    - @omega-web-components/wrapper
+	 *    - @omega-web-components/dialog
+	 * - @omega-mobile-components
+	 *    -
+	 * - @omega-web-containers
+	 *    - @omega-web-containers/chat
+	 * - @omega-state-machines
+	 *    - @omega-state-machines/chat
+	 * - @omega-services
+	 *    -
+	 * - @omega-utils
+	 *    -
+	 * - @omega-core
+	 *    - @omega-core/utils
+	 *    - @omega-core/services
+	 *    - @omega-core/assets
+	 *    - @omega-core/rules
+	 *    - @omega-core/config
+	 * - @omega-shared-components
+	 *    - @omega-shared-components/content
+	 * */
+}
---
+++ src/client/web/app/containers/login/login.components.ts
@@ -0,0 +1,3 @@
+// Components
+
+export const LoginComponentsEngine = {};
