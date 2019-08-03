2624b50115e72d8e325b749aa7e3d15a818afe19
Vipin <6302771+vip-git@users.noreply.github.com>
2019-08-03 22:03:49 +0200

    fix: new features added for omega packager

--- scripts/config/paths.js
+++ scripts/config/paths.js
@@ -4,6 +4,50 @@ const fs = require('fs');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
+const lernaPackages = {
+	containers: {
+		webContainers: [
+			{
+				chat: resolveApp('src/client/web/app/containers/chat'),
+				login: resolveApp('src/client/web/app/containers/login')
+			}
+		],
+		mobileContainers: [
+			{
+				chat: resolveApp('src/client/mobile/app/containers/chat'),
+				login: resolveApp('src/client/mobile/app/containers/login')
+			}
+		]
+	},
+	components: {
+		webComponents: [
+			{
+				'chat-box': resolveApp('src/client/web/app/common/components/chat-box'),
+				'doc-gen': resolveApp('src/client/web/app/common/components/doc-gen'),
+				wrapper: resolveApp('src/client/web/app/common/components/wrapper'),
+				dialog: resolveApp('src/client/web/app/common/components/dialog')
+			}
+		],
+		mobileComponents: [{
+
+        }],
+		sharedComponents: [{
+            content: resolveApp('src/client/shared/components/content'),
+        }]
+	},
+	rules: resolveApp('src/client/shared/rules'),
+	services: resolveApp('src/client/shared/services'),
+	state: [
+		{
+			chat: resolveApp('src/client/shared/rules'),
+			login: resolveApp('src/client/shared/rules')
+		}
+	],
+	config: resolveApp('src/client/shared/config'),
+	assets: resolveApp('src/client/shared/assets'),
+	utils: resolveApp('src/client/shared/utils')
+};
+
 const paths = {
     clientBuild: resolveApp('docker/frontend/prod/build/client'),
     serverBuild: resolveApp('docker/frontend/prod/build/server'),
@@ -16,6 +60,7 @@ const paths = {
     srcFrontEndModules: resolveApp('src/client/web/node_modules'),
     srcFrontEndChatModules: resolveApp('src/client/web/app/containers/chat/node_modules'),
     publicPath: '/static/',
+    lernaPackages,
 };
 
 paths.resolveModules = [
--- scripts/omega/packager.js
+++ scripts/omega/packager.js
@@ -1,4 +1,8 @@
+// Library
 const shell = require('shelljs');
+
+// config
+const paths = require('../config/paths');
 
 /**
  * Packages to deploy :
@@ -8,12 +12,46 @@ const shell = require('shelljs');
  * - src/client/shared/rules/package.json                    (increment and deploy)
  * - src/client/shared/services/package.json                 (increment and deploy)
  */
-const deployNewlyGeneratedContainerLernaPackages = () => {
+const deployAssociatedContainerLernaPackages = (containerName) => {
 
 };
 
-const incrementAllLernaPackages = () => {
-    
+/***
+ * Lerna repos
+ * - @omega-web-components
+ *    - @omega-web-components/chat-box
+ *    - @omega-web-components/doc-gen
+ *    - @omega-web-components/wrapper
+ *    - @omega-web-components/dialog
+ * - @omega-mobile-components
+ *    -
+ * - @omega-web-containers
+ *    - @omega-web-containers/chat
+ * - @omega-state-machines
+ *    - @omega-state-machines/chat
+ * - @omega-services
+ *    -
+ * - @omega-utils
+ *    -
+ * - @omega-core
+ *    - @omega-core/utils
+ *    - @omega-core/services
+ *    - @omega-core/assets
+ *    - @omega-core/rules
+ *    - @omega-core/config
+ * - @omega-shared-components
+ *    - @omega-shared-components/content
+ * */
+const incrementAllLernaPackages = (version) => {
+    const {
+        components: { webComponents, mobileComponents, sharedComponents },
+        containers: { webContainers, mobileContainers },
+        services,
+        rules,
+        config,
+        utils,
+        assets,
+     } = paths.lernaPackages;
 };
 
 
