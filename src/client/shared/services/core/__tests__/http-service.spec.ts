3109c0ead7ad2a8e7f0b92369a2a7899a029eaee
Vipin <6302771+vip-git@users.noreply.github.com>
2019-09-29 15:01:03 +0200

    fix: issues resolved with http service tests

--- src/client/shared/services/core/__tests__/http-service.spec.ts
+++ src/client/shared/services/core/__tests__/http-service.spec.ts
@@ -6,23 +6,28 @@ describe('HTTP Service', () => {
         const payload = {};
         const restApiCall = await HttpService.buildRestApiCall(
             'api', 'GET', '/cats', 'test', payload
-        ).toPromise();
-        expect(restApiCall).toBeDefined();
-        expect(restApiCall).toStrictEqual({
+        );
+        restApiCall.subscribe((responseApiCall) => {
+            expect(responseApiCall).toBeDefined();
+            expect(responseApiCall).toStrictEqual({
                 message: 'API call failed',
                 error: true,
                 payload,
+            });
         });
     });
     it('can handle errors', async () => {
         const payload = 'asdasd';
         const restApiCall = await HttpService.buildRestApiCall(
             'apiasdasd', 'GEsT', '/caats/asd', 'testasdsd', payload
-        ).toPromise();
-        expect(restApiCall).toStrictEqual({
+        );
+        restApiCall.subscribe((responseApiCall) => {
+            expect(responseApiCall).toBeDefined();
+            expect(responseApiCall).toStrictEqual({
                 message: 'API call failed',
                 error: true,
                 payload,
+            });
         });
     });
 });
