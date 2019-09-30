6bac46fe778ce17106c76329a0ff9650768085e3
Vipin <6302771+vip-git@users.noreply.github.com>
2019-09-30 14:05:02 +0200

    fix: e2e test setup with jest and puppeteer

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-  globalSetup: '<rootDir>/scripts/config/jest/puppeteer/setup.js',
-  globalTeardown: '<rootDir>/scripts/config/jest/puppeteer/teardown.js',
-  testEnvironment: '<rootDir>/scripts/config/jest/puppeteer/config.js',
-  testMatch: [
-    '<rootDir>/src/client/**/__tests__/**/*.e2e-test.{js,jsx,mjs}',
-    '<rootDir>/src/client/**/?(*.)(e2e-test).{js,jsx,mjs}',
-  ],
-  transform: {
-    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
-    '^.+\\.css$': '<rootDir>/scripts/config/jest/cssTransform.js',
-    '^(?!.*\\.(js|jsx|mjs|css|json)$)': '<rootDir>/scripts/config/jest/fileTransform.js',
-  },
+	globalSetup: '<rootDir>/scripts/config/jest/puppeteer/setup.js',
+	globalTeardown: '<rootDir>/scripts/config/jest/puppeteer/teardown.js',
+	testEnvironment: '<rootDir>/scripts/config/jest/puppeteer/config.js',
+	testMatch: [
+		'<rootDir>/src/client/**/__tests__/**/*.e2e-test.{js,jsx,ts,tsx,mjs}',
+		'<rootDir>/src/client/**/?(*.)(e2e-test).{js,jsx,ts,tsx,mjs}'
+	],
+	transform: {
+		'^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+		'^.+\\.(ts|tsx|mjs)$': '<rootDir>/node_modules/ts-jest',
+		'^.+\\.css$': '<rootDir>/scripts/config/jest/cssTransform.js',
+		'^(?!.*\\.(js|jsx|mjs|css|json)$)':
+			'<rootDir>/scripts/config/jest/fileTransform.js'
+	}
 };
--- src/client/web/app/__tests__/app.e2e-test.ts
+++ src/client/web/app/__tests__/app.e2e-test.ts
@@ -1,29 +1,21 @@
 /* eslint-disable */
 /* ignore coverage */
-const faker = require('faker');
-/* ignore coverage */
 const puppeteer = require('puppeteer');
-/* ignore coverage */
-const person = {
-	name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-	email: faker.internet.email(),
-	phone: faker.phone.phoneNumber(),
-	message: faker.random.words(),
-};
+
 /* ignore coverage */
 describe('H2 Text', () => {
 	test('h2 loads correctly', async () => {
 		const browser = await puppeteer.launch({
-			headless: false,
+			headless: false
 		});
 		const page = await browser.newPage();
 
 		page.emulate({
 			viewport: {
 				width: 500,
-				height: 2400,
+				height: 2400
 			},
-			userAgent: '',
+			userAgent: ''
 		});
 
 		await page.goto('http://localhost:8500/');
