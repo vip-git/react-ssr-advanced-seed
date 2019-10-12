c4670f9cc6185e6ff2925c2e7ad25abf56d6a0bb
Vipin <6302771+vip-git@users.noreply.github.com>
2019-10-12 20:31:15 +0200

    fix: service workers issues resolved

--- src/client/shared/assets/service-worker.js
+++ src/client/shared/assets/service-worker.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
 self.addEventListener('install', event => {
-	console.log('👷', 'install', event);
+	 event.waitUntil(
+		(async function() {
+			const cache = await caches.open('static-v1');
+			await cache.addAll(['offline.html', 'styles.css']);
+		})()
+	);
 	self.skipWaiting();
 });
 
@@ -12,5 +17,27 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', function(event) {
 	// console.log('👷', 'fetch', event);
-	event.respondWith(fetch(event.request));
+	  const { request } = event;
+		// Always bypass for range requests, due to browser bugs
+		if (request.headers.has('range')) return;
+		event.respondWith(
+			(async function() {
+				// Try to get from the cache:
+				const cachedResponse = await caches.match(request);
+				if (cachedResponse) return cachedResponse;
+
+				try {
+					// Otherwise, get from the network
+					return await fetch(request);
+				}
+ 				catch (err) {
+					// If this was a navigation, show the offline page:
+					if (request.mode === 'navigate') {
+						return caches.match('offline.html');
+					}
+					// Otherwise throw
+					throw err;
+				}
+			})()
+		);
 });
