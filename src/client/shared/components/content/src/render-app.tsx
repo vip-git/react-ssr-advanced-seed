a2cfea5c276d61d50c6e0b50fea01e504dc1dd0c
Vipin <6302771+vip-git@users.noreply.github.com>
2019-10-20 12:01:57 +0200

    fix: updated all package version

--- src/client/shared/components/content/src/render-app.tsx
+++ src/client/shared/components/content/src/render-app.tsx
@@ -1,23 +1,33 @@
 // Library
-import React, { ReactType } from 'react';
+import React from 'react';
 
 // Interface
 import { ISharedComponentProps } from './shared-component.interface';
 
 export default class App extends React.Component<ISharedComponentProps, {}> {
   render() {
-    const { View, Text } = this.props.api;
+    const { api } = this.props;
+    const { View, Text } = api;
     const { style } = this.props;
     return (
       <View
         style={{
           flex: 1,
           justifyContent: 'center',
+          alignItems: 'center',
           backgroundColor: 'skyblue',
           ...style
         }}
       >
-        <Text style={{ textAlign: 'center' }}>This is Shared Component!</Text>
+        <Text 
+          style={{
+            textAlign: 'center',
+            fontSize: '1.2em',
+            width: '75%',
+          }}
+        >
+          This is a Shared Component Built using React Native Web!
+        </Text>
       </View>
     );
   }
