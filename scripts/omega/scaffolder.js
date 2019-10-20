afc6fae5aebe12e73268245d71f3cee3046a8f26
Vipin <6302771+vip-git@users.noreply.github.com>
2019-10-20 12:49:22 +0200

    feat: initial version of scaffolder documented

---
+++ scripts/omega/scaffolder.js
@@ -0,0 +1,32 @@
+/***************************************************************************
+ * ----------------------------------------------------------------------***
+ *  version 0.1                                                        *****
+ * ----------------------------------------------------------------------***
+ * @description                                                        *****
+ * --------------------------------------------------------------------*****
+ * This is the first version or iteration at automating                *****
+ * frontend apps from scratch.                                         *****
+ * ----------------------------------------------------------------------***
+ * Current Assumptions (later to support more) :                       *****
+ * - You are going with React Stack                                    *****
+ *   (React - Redux (Observable) - Material UI)                        *****
+ * - Later it should be possible to generate complete generic solution *****
+ *   like eg: React - Redux (optional / saga / etc) - AntDesign        *****
+ ***************************************************************************
+ * How does it work :                                                  *****
+ ***************************************************************************
+ * It starts from the root of every project :                            ***
+ * eg:                                                                   ***
+ *  ./unimod.json (Holds the original information to scaffold)           ***
+ *  Generally 3 important things :                                       ***
+ *  - Config                                                             ***
+ *    - Web                                                              ***
+ *    - Mobile                                                           ***
+ *  - Routes (Web and Mobile)                                            ***
+ *    - Chat (Example)                                                   ***
+ *  - Containers                                                         ***
+ *    - Chat                                                             ***
+ *      - Web      (This will be then follow up to scaffold to more info)***
+ *      - Mobile   (This will be then follow up to scaffold to more info)***
+ ***************************************************************************/
+
