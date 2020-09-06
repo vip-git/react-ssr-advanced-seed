5a8869de5d8e838b405dd7796346aa170c48a1e6
Vipin Tanna <6302771+vip-git@users.noreply.github.com>
2020-09-06 21:40:55 +0200

    Update plugins.js

--- scripts/config/webpack.config.js/plugins.js
+++ scripts/config/webpack.config.js/plugins.js
@@ -61,7 +61,7 @@ const client = [
 	new CopyWebpackPlugin([
 		{
 			from: path.resolve(__dirname, '../../../_redirects'),
-			to: path.resolve(__dirname, '../../../docker/frontend/prod/build/client')
+			to: path.resolve(__dirname, '../../../docker/frontend/prod/build/client/_redirects')
 		}
 	]),
 	new CopyWebpackPlugin([
